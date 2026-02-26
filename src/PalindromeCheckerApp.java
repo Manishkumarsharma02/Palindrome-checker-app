847a36622e1dd339f0a34eba2cd496f268d26325
Manishkumarsharma02 <manish5347j@gmail.com>
2026-02-26 16:16:15 +0530

    UC6: Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,33 +4,30 @@ UC1:Palindrome checker app
 @author Manish
 @version 1.0
 */
-import  java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
-       public static  void main(String[] args){
-           System.out.println("input:");
-           Scanner scanner=new Scanner(System.in);
-           String original =(scanner.next());
-           String Reverse ="";
-           boolean present=false;
-           for (int i=original.length()-1;i>=0;i--){
-               Reverse=Reverse+original.charAt(i);
-           }
-           for (int i=0;i<original.length();i++){
-               if(original.charAt(i)==Reverse.charAt(i)){
-                   present=true;
-               }else {
-                   present=false;
-               }
-           }
-           if(present==true){
-               System.out.println(original+" and "+Reverse+" are Palindrome");
-           }else{
-               System.out.println(original+" and "+Reverse+" are not  Palindrome");
-           }
+    public static void main(String[] args){
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+        boolean isPalindrome = true;
+        while (!queue.isEmpty()) {
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
 
-
-
-       }
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
-
