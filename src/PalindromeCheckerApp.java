3e40c22370622334ac9ed74a2e22e2906effe478
Manishkumarsharma02 <manish5347j@gmail.com>
2026-02-26 16:40:32 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,33 +4,26 @@ UC1:Palindrome checker app
 @author Manish
 @version 1.0
 */
-import  java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class PalindromeCheckerApp {
-       public static  void main(String[] args){
-           System.out.println("input:");
-           Scanner scanner=new Scanner(System.in);
-           String original =(scanner.next());
-           String Reverse ="";
-           boolean present=false;
-           for (int i=original.length()-1;i>=0;i--){
-               Reverse=Reverse+original.charAt(i);
-           }
-           for (int i=0;i<original.length();i++){
-               if(original.charAt(i)==Reverse.charAt(i)){
-                   present=true;
-               }else {
-                   present=false;
-               }
-           }
-           if(present==true){
-               System.out.println(original+" and "+Reverse+" are Palindrome");
-           }else{
-               System.out.println(original+" and "+Reverse+" are not  Palindrome");
-           }
-
-
-
-       }
+    public static void main(String[] args){
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
 }
-
